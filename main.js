596c25f55f8352c06cf6327672f099c2e96a229c
Adam Bazzi <8884041+Abazzi@users.noreply.github.com>
2023-11-19 13:36:18 -0500

    Added catppuccin colours for work mode added hhkbGuy img added dataset work properties started working on switch function to switch modes removed src from picture

--- main.js
+++ main.js
@@ -8,3 +8,18 @@ const dateOptions = {
 };
 
 dateSpan.textContent = date.toLocaleDateString(undefined, dateOptions);
+
+function switchPicture() {
+  const pic = document.getElementById('picture');
+  if (pic.dataset.picture == 'bonfire') {
+    pic.dataset.picture == 'work';
+  } else {
+    pic.dataset.picture == 'bonfire';
+  }
+}
+
+window.addEventListener('keydown', (e) => {
+  if (e.key == 'w') {
+    switchPicture();
+  }
+});
