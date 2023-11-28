b7b23f420576dd78de61cba367068970fe70b031
Adam Bazzi <8884041+Abazzi@users.noreply.github.com>
2023-11-28 10:10:51 -0500

    json file is now one array with links, each object has mode property continued to work on creating link elements

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-async function fetchLinks() {
-  const response = await fetch('links.json');
+async function fetchLinks(url) {
+  const response = await fetch(url);
   return await response.json();
 }
 const dateSpan = document.querySelector('[data-date]');
@@ -11,12 +11,12 @@ const dateOptions = {
   month: 'long',
   day: 'numeric',
 };
-const links = fetchLinks();
-links.then((data) => {
-  console.log(data.bonfire);
-});
-
 dateSpan.textContent = date.toLocaleDateString(undefined, dateOptions);
+
+const links = fetchLinks('links.json');
+links.then((data) => {
+  console.log(data.work[0].group);
+});
 
 function switchPicture() {
   const pic = document.getElementById('picture');
@@ -29,91 +29,137 @@ function switchPicture() {
   }
 }
 
-console.log(linkGroups);
-
-function switchLinks(el) {
-  if (el.dataset.list == 'bonfire') {
+function appendToUL(group, data) {
+  if (group.dataset.list == 'bonfire') {
     clearChildNodes(el);
+    switch (data.group) {
+      case '0':
+        linkGroups[0].appendChild(createBonfireLinkEl(data));
+        break;
+      case '1':
+        linkGroups[1].appendChild(createBonfireLinkEl(data));
+        break;
+      case '2':
+        linkGroups[1].appendChild(createBonfireLinkEl(data));
+        break;
+    }
+  } else {
+    clearChildNodes(el);
+    switch (data.group) {
+      case '0':
+        linkGroups[0].appendChild(createWorkLinkEl(data));
+        break;
+      case '1':
+        linkGroups[1].appendChild(createWorkLinkEl(data));
+        break;
+      case '2':
+        linkGroups[1].appendChild(createWorkLinkEl(data));
+        break;
+    }
   }
-}
 
-function createWorkLinkEl(obj) {
-  const li = document.createElement('li');
-  const a = document.createElement('a');
+  function switchLinks(el) {
+    const links = fetchLinks('links.json');
+    links.then((data) => {
+      appendToUL(el, data);
+    });
+  }
 
-  li.setAttribute('data-list', 'work');
-  a.setAttribute('data-list', 'work');
-  a.setAttribute('href', obj.url);
-  a.setAttribute('target', '_blank');
-  a.textContnet = obj.name;
-  li.appendChild(a);
-  return li;
-}
+  function createWorkLinkEl(obj) {
+    const li = document.createElement('li');
+    const a = document.createElement('a');
 
-function createBonfireLinkEl(obj) {
-  const li = document.createElement('li');
-  const a = document.createElement('a');
-  li.setAttribute('data-list', 'bonfire');
-  a.setAttribute('data-list', 'bonfire');
-  a.setAttribute('href', obj.url);
-  a.setAttribute('target', '_blank');
-  a.textContnet = obj.name;
-  li.appendChild(a);
-  return li;
-}
+    li.setAttribute('data-list', 'work');
+    a.setAttribute('data-list', 'work');
+    a.setAttribute('href', obj.url);
+    a.setAttribute('target', '_blank');
+    a.textContnet = obj.name;
+    li.appendChild(a);
+    return li;
+  }
 
-function switchLists() {
-  const listItems = document.querySelectorAll('[data-list]');
-  listItems.forEach((li) => {
-    if (li.dataset.list == 'bonfire') {
-      li.removeAttribute('data-list');
-      li.setAttribute('data-list', 'work');
+  function createLI(obj){
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    if (){
+
+    }
+
+    li.setAttribute('data-list', 'work');
+    a.setAttribute('data-list', 'work');
+    a.setAttribute('href', obj.url);
+    a.setAttribute('target', '_blank');
+    a.textContnet = obj.name;
+    li.appendChild(a);
+    return li;
+
+  }
+
+  function createBonfireLinkEl(obj) {
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    li.setAttribute('data-list', 'bonfire');
+    a.setAttribute('data-list', 'bonfire');
+    a.setAttribute('href', obj.url);
+    a.setAttribute('target', '_blank');
+    a.textContnet = obj.name;
+    li.appendChild(a);
+    return li;
+  }
+
+  function switchLists() {
+    const listItems = document.querySelectorAll('[data-list]');
+    listItems.forEach((li) => {
+      if (li.dataset.list == 'bonfire') {
+        li.removeAttribute('data-list');
+        li.setAttribute('data-list', 'work');
+      } else {
+        li.removeAttribute('data-list');
+        li.setAttribute('data-list', 'bonfire');
+      }
+    });
+  }
+
+  function switchModes() {
+    const title = document.querySelector('[data-title]');
+    const infoBar = document.querySelector('[data-info-bar]');
+    const body = document.querySelector('[data-body]');
+    const directory = document.querySelector('[data-directory]');
+
+    if (title.dataset.title == 'bonfire') {
+      title.removeAttribute('data-title');
+      title.setAttribute('data-title', 'work');
+      infoBar.removeAttribute('data-info-bar');
+      infoBar.setAttribute('data-info-bar', 'work');
+      body.removeAttribute('data-body');
+      body.setAttribute('data-body', 'work');
+      directory.removeAttribute('data-directory');
+      directory.setAttribute('data-directory', 'work');
+      directory.innerHTML = '&gt; cd ~/work/<span class="blinking">_</span>';
     } else {
-      li.removeAttribute('data-list');
-      li.setAttribute('data-list', 'bonfire');
+      title.removeAttribute('data-title');
+      title.setAttribute('data-title', 'bonfire');
+      infoBar.removeAttribute('data-info-bar');
+      infoBar.setAttribute('data-info-bar', 'bonfire');
+      body.removeAttribute('data-body');
+      body.setAttribute('data-body', 'bonfire');
+      directory.removeAttribute('data-directory');
+      directory.setAttribute('data-directory', 'bonfire');
+      directory.innerHTML = '&gt; cd ~/bonfire/<span class="blinking">_</span>';
+    }
+  }
+
+  function clearChildNodes(el) {
+    while (el.firstChild) {
+      el.removeChild(el.firstChild);
+    }
+  }
+
+  window.addEventListener('keydown', (e) => {
+    if (e.key == 'w') {
+      switchPicture();
+      switchModes();
+      switchLists();
+      switchLinks();
     }
   });
-}
-
-function switchModes() {
-  const title = document.querySelector('[data-title]');
-  const infoBar = document.querySelector('[data-info-bar]');
-  const body = document.querySelector('[data-body]');
-  const directory = document.querySelector('[data-directory]');
-
-  if (title.dataset.title == 'bonfire') {
-    title.removeAttribute('data-title');
-    title.setAttribute('data-title', 'work');
-    infoBar.removeAttribute('data-info-bar');
-    infoBar.setAttribute('data-info-bar', 'work');
-    body.removeAttribute('data-body');
-    body.setAttribute('data-body', 'work');
-    directory.removeAttribute('data-directory');
-    directory.setAttribute('data-directory', 'work');
-    directory.innerHTML = '&gt; cd ~/work/<span class="blinking">_</span>';
-  } else {
-    title.removeAttribute('data-title');
-    title.setAttribute('data-title', 'bonfire');
-    infoBar.removeAttribute('data-info-bar');
-    infoBar.setAttribute('data-info-bar', 'bonfire');
-    body.removeAttribute('data-body');
-    body.setAttribute('data-body', 'bonfire');
-    directory.removeAttribute('data-directory');
-    directory.setAttribute('data-directory', 'bonfire');
-    directory.innerHTML = '&gt; cd ~/bonfire/<span class="blinking">_</span>';
-  }
-}
-
-function clearChildNodes(el) {
-  while (el.firstChild) {
-    el.removeChild(el.firstChild);
-  }
-}
-
-window.addEventListener('keydown', (e) => {
-  if (e.key == 'w') {
-    switchPicture();
-    switchModes();
-    switchLists();
-  }
-});
